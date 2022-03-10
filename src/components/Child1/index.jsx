a2e325cb658b35de9183941de1b7949c66411c82
Yagnesh Modh <hey@yagneshmodh.com>
2022-03-10 11:40:27 +0530

    basic react js completed

---
+++ src/components/Child1/index.jsx
@@ -0,0 +1,43 @@
+import React, { PureComponent } from 'react';
+// import shallowCompare from 'react-addons-shallow-compare';
+
+export default class index extends PureComponent {
+  //   shouldComponentUpdate(nextProps, nextState) {
+  //     return shallowCompare(this, nextProps, nextState);
+  //   }
+
+  componentDidMount() {
+    document.addEventListener('mousemove', this.mouseMove);
+    this.interval = setInterval(() => {
+      console.log('interval');
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener(
+      'mousemove',
+      this.mouseMove,
+    );
+    clearInterval(this.interval);
+  }
+
+  mouseMove = () => {
+    console.log('Mouse move');
+  };
+
+  render() {
+    console.log('Child 1 Component');
+    const { a } = this.props;
+    if (a >= 20) {
+      throw new Error(
+        'Value should not be greater then 20',
+      );
+    }
+    return (
+      <div>
+        Child 1 Component
+        <h3>Value of a from Child 1: {a}</h3>
+      </div>
+    );
+  }
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { Component, createRef } from 'react';
 import { render } from 'react-dom';
 import Header from './components/Header';
+import Child1 from './components/Child1';
 import './style.css';
+import ErrorWrapper from './components/ErrorWrapper';
 
 // Rules of React Component
 // 1. name of Component(react function) should be start with capital letter;
@@ -9,27 +11,200 @@ import './style.css';
 // 3. use inline style as object and object property should be in camel case;
 // 4. use classname instead of class as class is reserve word in javascript
 
-// Component
-const App = () => (
-  <div className="container">
-    <Header
-      title="Header"
-      listItems={['Home', 'About', 'Contact']}
-    />
-    <h1
-      style={{
-        backgroundColor: 'red',
-        color: 'green',
-      }}>
-      Hello
-    </h1>
-    <h2>hello world</h2>
-    <input type="checkbox" />
-    <Header
-      title="Title"
-      listItems={['Home', 'Projects', 'Testimonial']}
-    />
-  </div>
-);
+// Function Component
+// const App = () => (
+//   <div className="container">
+//     <Header
+//       title="Header"
+//       listItems={['Home', 'About', 'Contact']}
+//     />
+//     <h1
+//       style={{
+//         backgroundColor: 'red',
+//         color: 'green',
+//       }}>
+//       Hello
+//     </h1>
+//     <h2>Value of a: {a}</h2>
+//     <button
+//       type="button"
+//       onClick={() => {
+//         console.log('change A');
+//         a = 15;
 
-render(<App />, document.getElementById('root'));
+//         console.log(a);
+//       }}>
+//       Chagne A
+//     </button>
+//     <input type="checkbox" />
+//     <Header
+//       title="Title"
+//       listItems={['Home', 'Projects', 'Testimonial']}
+//     />
+//   </div>
+// );
+
+// 4 Stages of Lifecycle method
+
+// 1. Mounting
+// -> Constructor(call only once)
+//      1. define state value
+//      2. Analytics
+//      3. Define state value based on prop value
+
+// Note: cant set state value async
+
+// -> getDerivedStateFromProps
+//      1. derive new State value based on new Props and new State
+
+// -> render
+//     1. convert htm to virtual dom  -> actual dom
+
+// -> componentDidMount
+//    1. manipulate dom
+//    2. load data on component mount
+//    3. register events
+
+//  Note: Call only once
+
+// 2. Updating
+
+// 1. getDerivedStateFromProps
+
+// 2. shouldComponentUpdate / Pure Component / Memo
+
+// 3. render
+
+// 4. getSnapshotBeforeUpdate
+
+// 5. componentDidUpdate
+
+// 2.
+
+// 3. Unmounting
+
+// 1. componentWillUnmount
+
+// 4. Error
+
+// When Prop value change or state value change then and then render method will call
+class App extends Component {
+  listItem = ['Home', 'About', 'Contact'];
+
+  // Props are initial Props
+  constructor(props) {
+    super(props);
+    this.state = {
+      a: 15,
+      b: 10,
+      // greet: `Hello ${props.name}`,
+    };
+
+    this.h1Ref = createRef();
+
+    console.log('constructor');
+
+    // make api call get data
+    // set data as state;
+
+    //
+  }
+
+  // props are new Props
+  // state are new State
+  static getDerivedStateFromProps(props, state) {
+    console.log('getDerivedStateFromProps');
+    // console.log(props);
+    // console.log(state);
+    return {
+      greet: `Hello ${props.name}`,
+    };
+  }
+
+  // manipulate Dom element
+  // on component load fetch the data from api and displauy
+  // Register events
+  // Note: call only once
+  componentDidMount() {
+    // O(logN)
+    // O(N)
+
+    // O(1)
+    this.h1Ref.current.style = 'color: yellow';
+
+    document.addEventListener('copy', () => {
+      console.log('Copied');
+    });
+
+    // make api call get data
+    // set data as set state
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    return 10;
+  }
+
+  // Manipulate Dom element
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('snapshot', snapshot);
+  }
+
+  changeA = () => {
+    this.setState(({ a }) => ({ a: a + 5 }));
+  };
+
+  changeB = () => {
+    this.setState(({ b }) => ({ b: b + 5 }));
+  };
+
+  render() {
+    const { a, b, greet } = this.state;
+    const { name } = this.props;
+
+    console.log('render');
+
+    return (
+      <ErrorWrapper>
+        <div className="container">
+          <Header
+            title="Header"
+            listItems={this.listItem}
+          />
+          <ErrorWrapper>
+            {a < 25 && (
+              <Child1 a={a} listItems={this.listItem} />
+            )}
+          </ErrorWrapper>
+          <h1
+            ref={this.h1Ref}
+            id="heading"
+            style={{
+              backgroundColor: 'red',
+              color: 'green',
+            }}>
+            {greet}
+          </h1>
+          <h2>value of a: {a}</h2>
+          <h2>value of b: {b}</h2>
+          <button type="button" onClick={this.changeA}>
+            Change A
+          </button>
+          <button type="button" onClick={this.changeB}>
+            Change B
+          </button>
+          <input type="checkbox" />
+          <Header title="Title" listItems={this.listItem} />
+        </div>
+      </ErrorWrapper>
+    );
+  }
+}
+
+render(
+  <App name="Yagnesh" />,
+  document.getElementById('root'),
+);
