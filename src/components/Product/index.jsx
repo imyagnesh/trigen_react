8b36c9a601ec6019661aace570a515e65e78e6fa
Yagnesh Modh <hey@yagneshmodh.com>
2022-03-17 11:58:09 +0530

    completed basic api integration for products page

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import Rating from '../Rating';
 
-const Product = ({ product }) => (
+const Product = ({
+  product,
+  addToCart,
+  cartItem,
+  updateQuantity,
+  deleteCartItem,
+}) => (
   <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 py-6">
     <div className="w-full grid grid-cols-1 gap-y-8 gap-x-6 items-start sm:grid-cols-12 lg:gap-x-8">
       <div className="aspect-w-2 aspect-h-3 rounded-lg bg-gray-100 overflow-hidden sm:col-span-4 lg:col-span-3">
@@ -41,11 +47,46 @@ const Product = ({ product }) => (
             Product options
           </h3>
 
-          <button
-            type="submit"
-            className="mt-6 w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-            Add to bag
-          </button>
+          {cartItem ? (
+            <div className="flex items-center mt-6">
+              <button
+                type="button"
+                onClick={() =>
+                  updateQuantity({
+                    ...cartItem,
+                    quantity: cartItem.quantity + 1,
+                  })
+                }
+                className=" w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                +
+              </button>
+              <p className="mx-8 text-xl font-bold">
+                {cartItem.quantity}
+              </p>
+              <button
+                type="button"
+                onClick={() => {
+                  if (cartItem.quantity > 1) {
+                    updateQuantity({
+                      ...cartItem,
+                      quantity: cartItem.quantity - 1,
+                    });
+                  } else {
+                    deleteCartItem(cartItem);
+                  }
+                }}
+                className=" w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                -
+              </button>
+            </div>
+          ) : (
+            <button
+              type="button"
+              onClick={() => addToCart(product)}
+              className="mt-6 w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              Add to bag
+            </button>
+          )}
         </section>
       </div>
     </div>
--- src/pages/Main/Home/index.jsx
+++ src/pages/Main/Home/index.jsx
@@ -23,8 +23,49 @@ const Home = () => {
     }
   }, []);
 
-  console.log(products);
-  console.log(cart);
+  const addToCart = useCallback(async product => {
+    try {
+      const res = await axiosInstance.post('cart', {
+        quantity: 1,
+        productId: product.id,
+      });
+      setCart(prevVal => [...prevVal, res.data]);
+    } catch (error) {}
+  }, []);
+
+  const updateQuantity = useCallback(async cartItem => {
+    try {
+      const res = await axiosInstance.put(
+        `cart/${cartItem.id}`,
+        cartItem,
+      );
+      setCart(prevValue => {
+        const index = prevValue.findIndex(
+          x => x.id === cartItem.id,
+        );
+        return [
+          ...prevValue.slice(0, index),
+          res.data,
+          ...prevValue.slice(index + 1),
+        ];
+      });
+    } catch (error) {}
+  }, []);
+
+  const deleteCartItem = useCallback(async cartItem => {
+    try {
+      await axiosInstance.delete(`cart/${cartItem.id}`);
+      setCart(prevValue => {
+        const index = prevValue.findIndex(
+          x => x.id === cartItem.id,
+        );
+        return [
+          ...prevValue.slice(0, index),
+          ...prevValue.slice(index + 1),
+        ];
+      });
+    } catch (error) {}
+  });
 
   useEffect(() => {
     loadData();
@@ -32,9 +73,21 @@ const Home = () => {
 
   return (
     <div>
-      {products.map(product => (
-        <Product key={product.id} product={product} />
-      ))}
+      {products.map(product => {
+        const cartItem = cart.find(
+          x => x.productId === product.id,
+        );
+        return (
+          <Product
+            key={product.id}
+            product={product}
+            cartItem={cartItem}
+            addToCart={addToCart}
+            updateQuantity={updateQuantity}
+            deleteCartItem={deleteCartItem}
+          />
+        );
+      })}
     </div>
   );
 };
